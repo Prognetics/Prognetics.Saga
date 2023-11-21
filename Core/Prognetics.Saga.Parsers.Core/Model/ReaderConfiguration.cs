f82bb9145d0c9abf26703de91d54a3254085b4d0
Szymon Welter <szymon.welter98@gmail.com>
2023-11-21 18:00:45 +0100

    cleanup

--- Core/Prognetics.Saga.Parsers.Core/Model/ReaderConfiguration.cs
+++ Core/Prognetics.Saga.Parsers.Core/Model/ReaderConfiguration.cs
@@ -2,11 +2,12 @@
 {
     public class ReaderConfiguration
     {
-        public string ParserType { get; set; }
+        public string ParserType { get; set; } = string.Empty;
 
-        public string Path { get; set; }
+        public string Path { get; set; } = string.Empty;
 
         public bool TrackingEnabled { get; set; }
+
         public TimeSpan TrackingInterval { get; set; } = TimeSpan.FromMinutes(1);
     }
 }
